6faa5eb3d14c06506a08a4e77a552cfb07e1ef3a
hw20181102929 <51589984+hw20181102929@users.noreply.github.com>
2019-06-26 19:30:04 +0800

    Update main.cpp

--- hanshu/main.cpp
+++ hanshu/main.cpp
@@ -1,15 +1,26 @@
-//
-//  main.cpp
-//  hanshu
-//
-//  Created by s20181102929 on 2019/6/26.
-//  Copyright © 2019 s20181102929. All rights reserved.
-//
-
 #include <iostream>
+#include <algorithm>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int* bubble_sort(int n)/* 对长度为n的数组执行冒泡排序 */
+{
+    int *p=new int[n];
+    int i;
+    for(i=0;i<n;i++){
+        cin>>p[i];
+    }
+    sort(p,p+n);
+    return p;
+}
+int main()
+{
+    int n;
+    cin >> n;
+    int* a = bubble_sort(n);
+    for (int i = 0; i < n; i++) {
+        cout << a[i];
+        if (i < n - 1)cout << " ";
+    }
+    cout << endl;
     return 0;
 }
